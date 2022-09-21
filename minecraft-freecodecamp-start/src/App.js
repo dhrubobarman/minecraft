8523dd226eaa27cd61731ee188cb9fd7bd8255d1
dhrubobarman <dhrubobarman847@gmail.com>
2022-09-22 00:17:34 +0530

    changes

--- minecraft-freecodecamp-start/src/App.js
+++ minecraft-freecodecamp-start/src/App.js
@@ -1,11 +1,10 @@
-import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
+      <div>Outside Canvas</div>
       <Canvas>
-        <Sky />
       </Canvas>
     </>
   );
